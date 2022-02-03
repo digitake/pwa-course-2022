6f1a1829b39d657b6a4713836f1d05ccf7d9b23e
rattanakorn00 <rattanakorn.plan@bumail.net>
2022-02-03 14:55:16 +0700

    กลุ่ม

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+
+function Userlist () {
+  return (
+    <div className="userlist">
+      <h1>Chat</h1>
+    </div>
+  )
+}
+
+function Chat () {
+  return (
+    <div className="chat">
+      <h2>สวัสดี</h2>
+    </div>
+  )
+}
+
+function Inputbox () {
+  return (
+    <div className="inputbox">
+      <h2>หวัดดี</h2>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Userlist/>
+      <Chat/>
+      <Inputbox/>
+    </div>
+  );
+}
+
+export default App;
