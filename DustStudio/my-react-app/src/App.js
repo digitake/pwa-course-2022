97b2b6267a5bebe213bf2e965f7d6d26bc31462c
TANAWIT <tanawit.jaru@bumail.net>
2022-02-05 16:43:38 +0700

    Import beta app from "class/03/ex03".

--- DustStudio/my-react-app/src/App.js
+++ DustStudio/my-react-app/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Userlist () {
+  return (
+    <div className="userlist">
+    </div>
+  )
+}
+
+function Chat () {
+  return (
+    <div className="chat">
+    </div>
+  )
+}
+
+function Inputbox () {
+  return (
+    <div className="inputbox">
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <text class="Username">Username</text>
+      <Userlist/>
+      <p>Test Chatbox...</p>
+      <span class="time-right">11:00</span>
+      <Chat/>
+      <input type="text" placeholder="Message.."></input>
+      <a class="active" href="javascript:alert('Your message was send.')">Send</a>
+      <Inputbox/>
     </div>
   );
 }
