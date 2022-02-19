90dd005a9d5f7714c2de49abf52ab9868fb89af6
Thanadon-1630700563-GI472-229J <tanadon.noib@bumail.net>
2022-02-19 17:50:43 +0700

    Login UI Done

--- src/Login.js
+++ src/Login.js
@@ -3,10 +3,35 @@ import './css/Login.css'
 function Login(){
     return(
         <div>
-            <div className='TextSetting Text1'>A</div>
-            <div className='TextSetting Text2'>B</div>
-            <div className='TextSetting Text3'>C</div>
-            <div className='TextSetting Text1'>D</div>
+            <div 
+                className='BorderSetting01 Title'>
+                TextApp
+            </div>
+            <div 
+                className='BorderSetting01 TextBorder02'>
+                <div 
+                    className='BorderSetting01 PictureBorder'>
+                </div>
+                    <div className='BorderSetting02 TextSetting01'>
+                        Email
+                    </div>
+                <div className='BorderSetting01 LoginBorder'>
+                </div>
+                    <div className='BorderSetting02 TextSetting01'>
+                        Password
+                    </div>
+                <div className='BorderSetting01 LoginBorder'>
+                </div>
+                    <div className='BorderSetting02 TextSetting02'>
+                        Forgetting Password?
+                    </div>
+                <div className='BorderSetting01 TextSetting03 LoginBorder'>
+                    Login
+                </div>
+            </div>
+            <div 
+                className='BorderSetting01 TextBorder03'>
+            </div>
         </div>
     
     )
