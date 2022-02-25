14801b186e11510154802a6e335af3bcc055c603
Digitake <digitake@gmail.com>
2022-02-25 18:08:50 +0700

    Add the chatbox and its style

--- src/Chat.js
+++ src/Chat.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import App from './components/App.js';
 import Chatbox from './components/Chatbox.js';
 import Userlist from './components/Userlist.js';
@@ -5,11 +6,18 @@ import Inputbox from './components/Inputbox.js';
 import './Chat.css';
 
 function Chat() {
+  let [chatData, setChatData] = useState([
+    {user: "user1", msg: "Hello", position: "left"},
+    {user: "user2", msg: "Hi", position: "right"},
+    {user: "user1", msg: "How are you?", position: "left"},
+    {user: "user2", msg: "I'm fine, thank you. And you?", position: "right"},
+  ]);
+
   return (
     <App>
       <div className="chat">
         <Userlist/>
-        <Chatbox/>
+        <Chatbox data={chatData}/>
         <Inputbox/>
       </div>
     </App>
--- src/components/Chatbox.js
+++ src/components/Chatbox.js
@@ -1,6 +1,19 @@
-function Chatbox () {
+import Avatar from "./Avatar";
+import "../css/Chatbox.css";
+
+function Chatbox (props) {
+  let data = props.data;
+
   return (
     <div className="chatbox">
+      {
+        data.map((item) => {
+          return (<div className={`chatbox-item ${item.position}`}>
+            <Avatar name={item.user}/>
+            <div className="chatbox-item-text"> {item.msg} </div>
+          </div>)
+        })
+      }
     </div>
   )
 }
