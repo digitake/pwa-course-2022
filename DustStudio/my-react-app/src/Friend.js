ee7c48e8da0b44e2bf38e9378e23a77cd02f80de
TANAWIT <tanawit.jaru@bumail.net>
2022-02-25 19:43:26 +0700

    Update "Friend" webpage.

--- DustStudio/my-react-app/src/Friend.js
+++ DustStudio/my-react-app/src/Friend.js
@@ -1,40 +1,29 @@
-import './Friend'
+import './Friend.css'
+
+
 
 function Friend(){
     return(
         <div>
-           <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.." title="Type in a name">
                <ul id="myUL">
-                   <li><a href="#">Adele</a></li>
-                   <li><a href="#">Agnes</a></li>
-                   <li><a href="#">Billy</a></li>
-                   <li><a href="#">Bob</a></li>
-                   <li><a href="#">Calvin</a></li>
-                   <li><a href="#">Christina</a></li>
-                   <li><a href="#">Cindy</a></li>
+                   <li><a href="#">Friend1</a></li>
+                   <li><a href="#">Friend2</a></li>
+                   <li><a href="#">Friend3</a></li>
+                   <li><a href="#">Friend4</a></li>
+                   <li><a href="#">Friend5</a></li>
+                   <li><a href="#">Friend6</a></li>
+                   <li><a href="#">Friend7</a></li>
+                   <li><a href="#">Friend8</a></li>
+                   <li><a href="#">Friend9</a></li>
+                   <li><a href="#">Friend10</a></li>
+                   <li><a href="#">Friend11</a></li>
+                   <li><a href="#">Friend12</a></li>
+                   
                    </ul>
-                   </input>
+            
         </div>
         
     )
 }
-
-function myFunction() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
-
 
 export default Friend;
