27210153694657cbad8504014a8182211e8740e6
Looktorzz <kulisutt.j@gmail.com>
2022-02-25 20:15:10 +0700

    ลบ ; ที่เกิน

--- src/AddFriends.js
+++ src/AddFriends.js
@@ -3,7 +3,7 @@ import './AddFriends.css';
 function AddFriends () {
     return (
         <div className='body-add-friends'>
-            <div className="add-friends">Add Friends</div>
+            <div className="add-friends">+Friends</div>
             <div className="list-add-friends"></div>
             <div className="icon-add-friends"></div>
         </div>
