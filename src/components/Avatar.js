79f93d5453e63c90de0abfa9ac2c2006e428f3d8
azwaazwa <azwa.nikw@bumail.net>
2022-03-04 23:58:36 +0700

    Merge remote-tracking branch 'class-origin/class/07/ex01'

--- src/components/Avatar.js
+++
@@ -1,9 +0,0 @@
-import '../css/Avatar.css';
-
-export function Avatar(props) {
-  return (
-    <div className="avatar">
-      <img src={`https://avatars.dicebear.com/api/pixel-art/${props.name}.svg`} alt={props.name}/>
-    </div>
-  );
-}
