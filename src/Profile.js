17adfc385d0f3f00bdc1d3bec0fabfed68b2a3d5
p4nuruj <panuruj.innu@bumail.net>
2022-03-04 18:54:34 +0700

    Update Chatbox Userlist Profile

---
+++ src/Profile.js
@@ -0,0 +1,28 @@
+import App from './components/App.js';
+import {useState} from "react";
+import './Profile.css';
+import Avatar from './components/Avatar.js';
+
+function Profile() {
+  const [name, setName] = useState("Unknown");
+
+
+  return (
+    <App>      
+      <div className="userbox"/>
+      <div className="Profile">
+        <Avatar name={name}/>
+        <label className="label1" htmlFor="displayname"/>
+        <input 
+          id="displayname"
+          type="text" 
+          className="text" 
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      </div>
+    </App>
+  );
+}
+
+export default Profile;
