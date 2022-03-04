a86735c7bfd776e89616b413079901df37b9eeb5
NaitySan <nine_nine@outlook.com>
2022-03-04 17:55:15 +0700

    Test

---
+++ groupworkweb/src/FriendList.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import './FriendList.css';
+import App from './components/App';
+
+function FriendList(){
+  const [friends, setFriends] = useState([
+    {user: "user1", displayName: "A"},
+    {user: "user2", displayName: "B"},
+    {user: "user3", displayName: "C"},
+  ]);
+
+  return(
+    <App>
+      <div className="friend-list">
+       {friends.map(x => 
+       <div className='friend-list-item'>
+            <div>{friends.user}</div>
+        {x.displayName}
+         </div>
+       )
+       }
+      </div>
+    </App>
+  )
+}
+
+export default FriendList;
