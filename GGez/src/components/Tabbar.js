09acfa79064d8527822fe13e3565036c6ba57c02
azwaazwa <azwa.nikw@bumail.net>
2022-03-05 14:44:46 +0700

    เเก้งาน

--- GGez/src/components/Tabbar.js
+++ GGez/src/components/Tabbar.js
@@ -1,18 +1,20 @@
-import './css/Tabbar.css';
+import '../css/Tabbar.css';
 import { Link } from 'react-router-dom';
 
 function Tabbar () {
   return (
     <div className="tabbar">
       <Link to="/friend-list" className="tabbutton">
-        <i class="fa fa-envelope-open" aria-hidden="true"></i>
+        Friend List
       </Link>
       <Link to="/chat" className="tabbutton">
-        <i class="fa fa-bars" aria-hidden="true"></i>
+        Chat
       </Link>
       <Link to="/profile" className="tabbutton">
-        <i class="fa fa-play" aria-hidden="true"></i>
+        Profile
       </Link>
     </div>
   )
 }
+
+export default Tabbar;
