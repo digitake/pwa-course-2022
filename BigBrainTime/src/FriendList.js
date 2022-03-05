649610820107579c676eb782b9bcf42d183305a7
ArmNewbyDev <arm123009@gmail.com>
2022-03-05 14:52:50 +0700

    Updated Design and Add class/07 App for example for groupWork

---
+++ BigBrainTime/src/FriendList.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import './FriendList.css';
+import App from './components/App';
+import Avatar from './components/Avatar';
+import Userlist from './components/Userlist';
+
+
+function FriendList(){
+  const [friends, setFriends] = useState([
+    {user: "user1", displayName: "ไอ้ปื้ด"},
+    {user: "user2", displayName: "นางสาวสุดา"},
+    {user: "user3", displayName: "พลทหารหนึ่ง"},
+    {user: "user4", displayName: "ศาตราจารย์นายกรัฐมนตรีดอกเตอร์เขียว"},
+  ]);
+
+  return(
+    <App>
+      <div className="friend-list">
+       {
+         friends.map( x=> 
+
+            <div className="friend-list-item"> 
+            <Avatar name={x.user}/>{x.displayName}
+            </div>
+          )
+       
+      }
+      </div>
+    </App>
+  )
+}
+
+export default FriendList;
