f2e06bb8f6bea0a2ca9dbc45ada350e709c58c00
MR. KATAPAT KITTIKOVITTANA <katapat.kitt@gmail.net>
2022-03-06 00:24:30 +0700

    update 6/3/65

--- noobcode/src/Profile.js
+++ noobcode/src/Profile.js
@@ -13,73 +13,24 @@ function Profile() {
         <h1>My Profile</h1>
         <Avatar name={name}/>
         <label className="label1" htmlFor="displayname"/>
-        
-
-        <form class="row g-3 needs-validation" novalidate>
-          <div class="col-md-4">
-            <label for="validationCustom01" class="form-label">First name</label>
-               <input type="text" class="form-control" id="validationCustom01" required></input>
-              <div class="valid-feedback">
-                             Looks good!
-              </div>
-          </div>
-           <div class="col-md-4">
-            <label for="validationCustom02" class="form-label">Last name</label>
-            <input type="text" class="form-control" id="validationCustom02"  required></input>
-           <div class="valid-feedback">
-                Looks good!
-            </div>
-          </div>
-           <div class="col-md-4">
-        <label for="validationCustomUsername" class="form-label">Username</label>
-           <div class="input-group has-validation">
-             <span class="input-group-text" id="inputGroupPrepend">@</span>
-         <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required></input>
-             <div class="invalid-feedback">
-                Please choose a username.
-           </div>
-           </div>
-         </div>
-          <div class="col-md-6">
-          <label for="validationCustom03" class="form-label">City</label>
-            <input type="text" class="form-control" id="validationCustom03" required></input>
-          <div class="invalid-feedback">
-              Please provide a valid city.
-          </div>
+        <p></p>
+      <form>
+        <div class="mb-3">
+       <label for="exampleInputEmail1" class="form-label">Email address</label>
+       <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+       <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+       </div>
+       <div class="mb-3">
+       <label for="exampleInputPassword1" class="form-label">Password</label>
+       <input type="password" class="form-control" id="exampleInputPassword1"></input>
         </div>
-        <div class="col-md-3">
-          <label for="validationCustom04" class="form-label">State</label>
-         <select class="form-select" id="validationCustom04" required>
-           <option selected disabled value="">Choose...</option>
-           <option>ไทย</option>
-         </select>
-          <div class="invalid-feedback">
-           Please select a valid state.
-         </div>
-     </div>
-       <div class="col-md-3">
-         <label for="validationCustom05" class="form-label">Zip</label>
-         <input type="text" class="form-control" id="validationCustom05" required></input>
-          <div class="invalid-feedback">
-           Please provide a valid zip.
-          </div>
+        <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
         </div>
-      <div class="col-12">
-          <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required></input>
-          <label class="form-check-label" for="invalidCheck">
-             Agree to terms and conditions
-         </label>
-          <div class="invalid-feedback">
-             You must agree before submitting.
-            </div>
-         </div>
-        </div>
-         <div class="col-12">
-          <button class="btn btn-primary" type="submit">Submit form</button>
-        </div>
-      </form>
-
+        <button type="submit" class="btn btn-primary">Submit</button>
+       </form>
+       
 
 
         
