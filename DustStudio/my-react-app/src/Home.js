14301a0fdf627f2e1601bf00b0728dbb04cf7dd4
TANAWIT <tanawit.jaru@bumail.net>
2022-03-06 22:36:09 +0700

    Add more path in "Home" menu.

--- DustStudio/my-react-app/src/Home.js
+++ DustStudio/my-react-app/src/Home.js
@@ -9,13 +9,13 @@ function Home(){
                 <a href="javascript:void(0)" class="closebtn" onClick={closeNav}>&times;</a>
                 <a href="friend">Friends</a>
                 <a href="#">Groups</a>
-                <a href="#">Profile</a>
-                <a href="#">Contact</a>
+                <a href="profile">Profile</a>
+                <a href="app">Contact</a>
             </div>
             <div class="topnav">
                 <a onClick={openNav}>&#9776;</a>
                 <b></b>
-                <a class="active" href="#home">Home</a>
+                <a class="active" href="/">Home</a>
                 <a href="friend">Friends</a>
                 <a href="group">Groups</a>
                 <a href="profile">Profile</a>
