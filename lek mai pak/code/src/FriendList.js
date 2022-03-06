cff8133b25b8461dcc92a90d25e34943f5c5bc21
Thanon-GI472 <thanon.than@bumail.net>
2022-03-06 16:11:09 +0700

    Add searchbox

--- lek mai pak/code/src/FriendList.js
+++ lek mai pak/code/src/FriendList.js
@@ -2,25 +2,29 @@ import { useState } from 'react';
 import './FriendList.css';
 import App from './components/App';
 import Avatar from './components/Avatar';
+import SearchBox from './components/Searchbox';
 
 function FriendList(){
   const [friends, setFriends] = useState([
-    {user: "pop", displayName: "pop"},
-    {user: "bob", displayName: "bob"},
-    {user: "bab", displayName: "bab"},
-    {user: "peter", displayName: "peter"},
+    {user: "pop", displayName: "pop", msg: "hi"},
+    {user: "bob", displayName: "bob", msg: "Hello"},
+    {user: "bab", displayName: "bab", msg: "Yo!"},
+    {user: "peter", displayName: "peter", msg: "Whatsup!"},
   ]);
 
   return(
-    <App>
+    <App><SearchBox/>
       <div className="friend-list">        
         {
           friends.map ( x =>
               <div className='friend-list-item'>
                 <Avatar name={x.user}/> 
-                {x.displayName}                                              
+                 <div className='text'>
+                    {x.displayName}<br/><br/>
+                    {x.msg}
+                  </div>
               </div>
-            )          
+            )
         }        
       </div>
     </App>
---
+++ lek mai pak/code/src/components/Searchbox.js
@@ -0,0 +1,12 @@
+import '../css/Searchbox.css';
+
+function SearchBox () {
+    return (
+        <div className="SearchBox">
+            <div className='BGM'>
+            </div>
+    </div>
+    )
+}
+  
+export default SearchBox;
