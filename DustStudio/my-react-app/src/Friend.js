8bec3c1e88df7c3dd27ce1a952d25ab168170102
TANAWIT <tanawit.jaru@bumail.net>
2022-03-06 19:28:41 +0700

    Add "TopNav" in "Friends" website and add "FriendSearch" in "Friends" website.

--- DustStudio/my-react-app/src/Friend.js
+++ DustStudio/my-react-app/src/Friend.js
@@ -1,30 +1,69 @@
-import './Friend.css'
-
+import './Friend.css';
+import './TopNav.css';
+import './Menu.css';
+import {openNav,closeNav} from './Menu';
 
 
 
 function Friend(){
     return(
+
         <div>
-            <input type="text" id="mySearch" onkeyup="Friend()" placeholder="Search.." title="Type in a friend"></input>
-               <ul id="myUL">
-                   <li><a href="#1">Friend1</a></li>
-                   <li><a href="#2">Friend2</a></li>
-                   <li><a href="#3">Friend3</a></li>
-                   <li><a href="#4">Friend4</a></li>
-                   <li><a href="#5">Friend5</a></li>
-                   <li><a href="#6">Friend6</a></li>
-                   <li><a href="#7">Friend7</a></li>
-                   <li><a href="#8">Friend8</a></li>
-                   <li><a href="#9">Friend9</a></li>
-                   <li><a href="#10">Friend10</a></li>
-                   <li><a href="#11">Friend11</a></li>
-                   <li><a href="#12">Friend12</a></li>
-                </ul>  
+            <div id="mySidenav" class="sidenav">
+                <a href="javascript:void(0)" class="closebtn" onClick={closeNav}>&times;</a>
+                <a href="friend">Friends</a>
+                <a href="#">Groups</a>
+                <a href="#">Profile</a>
+                <a href="#">Contact</a>
+            </div>
+            <div class="topnav">
+                <a onClick={openNav}>&#9776;</a>
+                <b></b>
+                <a href="/">Home</a>
+                <a class="active" href="friend">Friends</a>
+                <a href="group">Groups</a>
+                <a href="profile">Profile</a>
+            </div>   
+
+                    
+            <div class="row">
+                <div class="left">
+                    <input type="text" id="mySearch" onKeyUp={myFunction} placeholder="Search.." title="Type in a category"></input>
+                     <ul id="myFriends">
+                         <li><a href="#">Friend1</a></li>
+                         <li><a href="#">Friend2</a></li>
+                         <li><a href="#">Friend3</a></li>
+                         <li><a href="#">Friend4</a></li>
+                         <li><a href="#">Friend5</a></li>
+                         <li><a href="#">Friend6</a></li>
+                         <li><a href="#">Friend7</a></li>
+                         <li><a href="#">Friend8</a></li>
+                         <li><a href="#">Friend9</a></li>
+                </ul>
+                </div>
+                <div class="right">
+                    <h2>Your Friends</h2>
+                </div>
+            </div>
         </div>
     )
  
 }
+function myFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("mySearch");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myFriends");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
 
 
 export default Friend;
--- DustStudio/my-react-app/src/Home.js
+++ DustStudio/my-react-app/src/Home.js
@@ -1,4 +1,4 @@
-import './Home.css';
+import './TopNav.css';
 import './Menu.css';
 import {openNav,closeNav} from './Menu';
 
@@ -7,21 +7,20 @@ function Home(){
         <div>
             <div id="mySidenav" class="sidenav">
                 <a href="javascript:void(0)" class="closebtn" onClick={closeNav}>&times;</a>
-                 <a href="friend">Friends</a>
-                 <a href="#">Groups</a>
-                 <a href="#">Profile</a>
-                 <a href="#">Contact</a>
+                <a href="friend">Friends</a>
+                <a href="#">Groups</a>
+                <a href="#">Profile</a>
+                <a href="#">Contact</a>
             </div>
             <div class="topnav">
-            <a onClick={openNav}>&#9776;</a>
-            <b></b>
-            <a class="active" href="#home">Home</a>
-            <a href="friend">Friends</a>
-            <a href="group">Groups</a>
-            <a href="profile">Profile</a>
+                <a onClick={openNav}>&#9776;</a>
+                <b></b>
+                <a class="active" href="#home">Home</a>
+                <a href="friend">Friends</a>
+                <a href="group">Groups</a>
+                <a href="profile">Profile</a>
             </div>   
         </div>
-
     )
 }
 
