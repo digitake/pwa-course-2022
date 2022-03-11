24e0abcd6fedd586759cfc247bb97d8c098a9028
HonTammarat <tammarat.ampo@bumail.net>
2022-03-11 16:57:31 +0700

    Edit profile page

--- 2T/src/Profile.js
+++ 2T/src/Profile.js
@@ -3,11 +3,11 @@ import './css/Profile.css'
 function Profile(){
   return(
     <div className='ProfileBoard'>
-      <div className='PB1 MenuTitle'>Auttony</div>
+      <div className='PB1'>Auttony</div>
       <div className='PB1 Your'>
         <div className='YourPicture'></div>
       </div>
-      <div className='PB1 Friends'>Friends</div>
+      <div className='Friends'>Friends</div>
       <div className='PB1 List'>
         <div className='FriendsPicture'></div>
         <div className='FriendsPicture'></div>
@@ -15,7 +15,7 @@ function Profile(){
         <div className='FriendsPicture'></div>
       
       </div>
-      <div className='PB1 TextBorder03'></div>
+      <div className='BottomTab'></div>
     </div>
   )
 }
