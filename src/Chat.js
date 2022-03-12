55422e5f1ec32f7e4b9c569555326b7e26673d4f
Digitake <digitake@gmail.com>
2022-03-12 14:23:40 +0700

    Minor update

--- src/Chat.js
+++ src/Chat.js
@@ -13,10 +13,10 @@ function Chat() {
   const { authState } = useAuthStateContext();
 
   let [chatData, setChatData] = useState([
-    {user: "user1", msg: "Hello", position: "left"},
-    {user: "user2", msg: "Hi", position: "right"},
-    {user: "user1", msg: "How are you?", position: "left"},
-    {user: "user2", msg: "I'm fine, thank you. And you?", position: "right"},
+    {user: "user1", msg: "Hello", position: "left", timestamp: 0},
+    {user: "user2", msg: "Hi", position: "right", timestamp: 0},
+    {user: "user1", msg: "How are you?", position: "left", timestamp: 0},
+    {user: "user2", msg: "I'm fine, thank you. And you?", position: "right", timestamp: 0},
   ]);
 
   const onMsg = (msg) => {
@@ -42,7 +42,7 @@ function Chat() {
       <Userlist />
       <div className="chat">
         <Titlebar value="Chat"/>        
-        <Chatbox data={chatData}/>
+        <Chatbox data={chatData.sort((a,b)=>a.timestamp-b.timestamp)}/>
         <Inputbox onEnter={x=>sendMsg(x,"mainhall")}/>
       </div>
     </App>
