203f1b1f0bd0a4d83c88253720773ca7e416b5a7
Digitake <digitake@gmail.com>
2022-03-18 22:36:58 +0700

    Implement Chatbox/Chatline

---
+++ src/components/Chatline.js
@@ -0,0 +1,44 @@
+import { useState,useEffect } from 'react';
+import { Link } from "react-router-dom";
+
+function Chatline (props) {
+  let text = props.text;
+
+  const [element, setElement] = useState(text);
+
+  function transform(text) {
+    if (text.startsWith("/link")) {
+      setElement(<Link to={text}>{text}</Link>);
+    }
+    else if (text.startsWith("/embed")) {
+      setElement(text);
+    } 
+    else if (text.startsWith("/age")) {
+      const [, name] = text.split(" ");
+      fetch("https://api.agify.io/?name="+name)
+        .then(res => res.json())
+        .then(data => setElement(data.age));
+    } 
+    else if (text.startsWith("/gender")) {
+      const [, name] = text.split(" ");
+      fetch("https://api.genderize.io/?name="+name)
+        .then(res => res.json())
+        .then(data => setElement(data.gender));
+    } 
+    else if (text.startsWith("/randomfact")) {
+      // ex10-01 here: Implement randomfact using fetch & promise
+      // https://uselessfacts.jsph.pl/random.json?language=en
+      
+    }
+  };
+
+  useEffect(() => {
+    transform(text);
+  },[]);
+
+  return (
+    <div dangerouslySetInnerHTML={{__html: element}}></div>
+  )
+}
+
+export default Chatline;
