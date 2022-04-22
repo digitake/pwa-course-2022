bb1313f9df12dc74b661cb2b522343b2a24da3a7
Thanadon-1630700563-GI472-229J <tanadon.noib@bumail.net>
2022-04-22 23:58:08 +0700

    Change Firebase

--- 2T/src/context/FirebaseConfig.js
+++ 2T/src/context/FirebaseConfig.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/database';
 const firebaseConfig = {
-  apiKey: "AIzaSyDOPJ9_X7MbrMXHkai922aLK2NVa7xbGjo",
-  authDomain: "gi472-pwa-course-2022.firebaseapp.com",
-  projectId: "gi472-pwa-course-2022",
-  storageBucket: "gi472-pwa-course-2022.appspot.com",
-  messagingSenderId: "967644458652",
-  appId: "1:967644458652:web:6f71ad83773786006b8759",
-  measurementId: "G-Z7Z74R200B",
-  databaseURL: "https://gi472-pwa-course-2022-default-rtdb.asia-southeast1.firebasedatabase.app"
+  apiKey: "AIzaSyDq-DWuipU39S7rRarmqHwgxik2sKUBF50",
+  authDomain: "t-textapp-pwa-coures-2022.firebaseapp.com",
+  databaseURL: "https://t-textapp-pwa-coures-2022-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "t-textapp-pwa-coures-2022",
+  storageBucket: "t-textapp-pwa-coures-2022.appspot.com",
+  messagingSenderId: "1091787927953",
+  appId: "1:1091787927953:web:60d434cdb7b616fc22aa30",
+  measurementId: "G-56T6M8L6FQ"
 };
 
 firebase.initializeApp(firebaseConfig);
