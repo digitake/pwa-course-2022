df0f6a292a70f39c544835d8698646e4cab2c2e6
supakit.thar <supakit.thar@bumail.net>
2022-04-23 09:55:49 +0700

    minor adjustment

--- colorful/src/context/FirebaseConfig.js
+++ colorful/src/context/FirebaseConfig.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/database';
 const firebaseConfig = {
-  apiKey: "AIzaSyDOPJ9_X7MbrMXHkai922aLK2NVa7xbGjo",
-  authDomain: "gi472-pwa-course-2022.firebaseapp.com",
-  projectId: "gi472-pwa-course-2022",
-  storageBucket: "gi472-pwa-course-2022.appspot.com",
-  messagingSenderId: "967644458652",
-  appId: "1:967644458652:web:6f71ad83773786006b8759",
-  measurementId: "G-Z7Z74R200B",
-  databaseURL: "https://gi472-pwa-course-2022-default-rtdb.asia-southeast1.firebasedatabase.app"
+  apiKey: "AIzaSyAmzhOiMjv9ip6sD_kKxFK0jhC4azhM6bA",
+  authDomain: "colorful-e68bd.firebaseapp.com",
+  projectId: "colorful-e68bd",
+  storageBucket: "colorful-e68bd.appspot.com",
+  messagingSenderId: "580124798189",
+  appId: "1:580124798189:web:0cf594375c591171f1f0e4",
+  measurementId: "G-LZ3C9EJGPD",
+  databaseURL: "https://colorful-e68bd-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
 
 firebase.initializeApp(firebaseConfig);
