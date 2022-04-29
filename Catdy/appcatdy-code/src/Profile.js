5b205e5be1df2b7a24fd8a83e7ca51054ace6c49
filmkb <chitsanupong.ssrik@bumail.net>
2022-04-29 16:50:51 +0700

    add login

--- Catdy/appcatdy-code/src/Profile.js
+++ Catdy/appcatdy-code/src/Profile.js
@@ -1,17 +1,53 @@
 import App from './components/App.js';
-import {useState} from "react";
+import { useState, useEffect } from "react";
 import './Profile.css';
 import Avatar from './components/Avatar.js';
+import { useAuthStateContext } from './context/FirebaseAuthContextProvider';
+import { useChatStateContext } from './context/FirebaseChatContextProvider.js';
+import FileBase64 from 'react-file-base64';
+
 
 function Profile() {
-  const [name, setName] = useState("Unknown");
+  const { authState, updateDisplayName, signOut } = useAuthStateContext();
+  const { setUserImage, getUserImage } = useChatStateContext();
+  const [name, setName] = useState(authState.user.displayName);
+  const [base64Image, setBase64Image] = useState("");
 
+  useEffect(() =>{
+    getUserImage(authState.user.uid).then(imgBase64 => {
+      setBase64Image(imgBase64);
+    })
+  }, []);
+
+  function save() {
+    updateDisplayName(name).then(_=>{
+      authState.user.reload()
+      setUserImage(authState.user.uid, base64Image);
+    });
+  }
+
+  function onDone(data) {
+    setBase64Image(data.base64);
+  }
 
   return (
     <App>
       <div className="Profile">
-        <div className="pro"> Mr.Chitsanupong Sri-kaew </div>
-        <div className="Ero"> User : filmkb_53@hotmail.com </div>
+        <Avatar name={name} base64Image={base64Image}/>
+        <label className="label1" htmlFor="displayname"/>
+        <input 
+          id="displayname"
+          type="text" 
+          className="text" 
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+        <FileBase64
+          multiple={ false }
+          onDone={ onDone } 
+        />
+        <input type="button" value="Save" onClick={save}/>
+        <input type="button" onClick={() => signOut()} value="Sign-out"/>
       </div>
     </App>
   );
--- Catdy/appcatdy-code/src/Router.js
+++ Catdy/appcatdy-code/src/Router.js
@@ -4,21 +4,37 @@ import {
   Route
 } from "react-router-dom";
 import Chat from "./Chat";
+import PrivateChat from "./PrivateChat";
 import Profile from "./Profile";
 import FriendList from "./FriendList";
 import Home from "./Home";
+import { useAuthStateContext, LoginComponent } from './context/FirebaseAuthContextProvider';
+import ChatStateProvider from "./context/FirebaseChatContextProvider";
 
 function Router() {
-  return (
+  const { authState } = useAuthStateContext();
+
+  if (authState && authState.state === "AUTHENTICATION_LOADING") {
+    return <div>Loading...</div>;
+  }
+  else if (authState && authState.state === "AUTHENTICATED") {
+    return (
+      <ChatStateProvider self={authState.user}>
     <BrowserRouter>
       <Routes>
         <Route path="/chat" element={<Chat />}/>
+        <Route path="/privatechat/:uid" element={<PrivateChat />}/>
         <Route path="/profile" element={<Profile />}/>
         <Route path="/home" element={<Home />}/>
         <Route path="/" element={<Chat />}/>
       </Routes>
     </BrowserRouter>
-  );
+    </ChatStateProvider>
+   );
+ } else {
+  return <LoginComponent />;
+ }
 }
+
 
 export default Router;
