31bf45461ee169c1fbc6c92718a23734aae04126
choeykeewong <palm19785@gmail.com>
2022-05-03 22:49:55 +0700

    อีกไม่นาน

--- src/index.js
+++ src/index.js
@@ -18,3 +18,11 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(registration => {
+    console.log('SW registered: ', registration);
+  })
+}
