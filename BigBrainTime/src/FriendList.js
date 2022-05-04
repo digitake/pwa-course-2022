9a1aaba76e59585ccf527880e7bb703f4ab44347
mookpnr <phatnaree.sukp@bumail.net>
2022-05-04 15:32:54 +0700

    Avatar IMG Update

--- BigBrainTime/src/FriendList.js
+++ BigBrainTime/src/FriendList.js
@@ -4,27 +4,9 @@ import App from './components/App';
 import Avatar from './components/Avatar';
 import { useChatStateContext } from './context/FirebaseChatContextProvider';
 import { Link } from 'react-router-dom';
-import { useAuthStateContext } from './context/FirebaseAuthContextProvider';
 
 function FriendList(){
   const { userList, imageDict, userDict } = useChatStateContext();
-  const { authState } = useAuthStateContext();
-
-  function showPics(item){
-    let displayName = "ไม่ทราบชื่อ(Offline)";
-    if (item.user in userDict && userDict[item.user].displayName){
-      displayName = userDict[item.user].displayName;
-    } else if (item.user === authState.user.uid) {
-      displayName = authState.user.displayName;
-    }
-    
-    return ({
-      ...item,
-      key: item.timestamp || Date.now(),
-      displayName: displayName,
-      image: imageDict[item.user] || "",
-    });
-  }
 
   const [favorite, setFavorite] = useState([
     {user: "user1", displayName: "ไอ้ปื้ด"},
@@ -50,10 +32,11 @@ function FriendList(){
        <div className="friend-icon">Friends</div>
        {
          userList.map((item) => {
+           let img = imageDict[item.key];
           return (
             <Link to={`/privatechat/${item.key}`} key={item.key}>
               <div className={`friend-list-item ${item.position}`}>
-                <Avatar name={item.displayName}/>
+                <Avatar name={item.displayName} base64Image={img}/>
                 <div>{item.displayName}</div>
               </div>
             </Link>
