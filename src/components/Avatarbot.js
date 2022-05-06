d47047fd5c579b4659fd82f3958c1255ed49332d
choeykeewong <palm19785@gmail.com>
2022-05-06 17:10:46 +0700

    add friend in userlist

--- src/components/Avatarbot.js
+++ src/components/Avatarbot.js
@@ -1,15 +1,5 @@
 import '../css/Avatar.css';
 
-export function Avatar(props) {
-  const base64Image = props.base64Image;
-  
-  return (
-    <div className="avatar">
-      <img src={base64Image} alt={props.name}/>
-    </div>
-  );
-}
-
 export function Avatarbot(props) {
   return (
     <div className="avatarbot">
@@ -18,5 +8,4 @@ export function Avatarbot(props) {
   );
 }
 
-export default Avatar;
-
+export default Avatarbot;
--- src/components/Userlist.js
+++ src/components/Userlist.js
@@ -1,18 +1,31 @@
 import { useState } from 'react';
 import '../css/Userlist.css';
+import Avatarbot from './Avatarbot';
+import '../css/Avatar.css';
 
 function Userlist () {
-  const [trayStatus, setTrayStatus] = useState("open");
+  const [friends] = useState([
+    {user: "user1", displayName: "ไอ้ปื้ด"},
+    {user: "user2", displayName: "นางสาวส้ม"},
+    {user: "user3", displayName: "พลทหารแดง"},
+    {user: "user4", displayName: "ศาตราจารย์นายกรัฐมนตรีดอกเตอร์เขียว"},
+  ]);
 
-  function onButtonClicked() {
-    setTrayStatus(oldStatus => oldStatus === "open" ? "closed" : "open");
-  }
-
-  return (
-    <div className={"userlist "+trayStatus}>
-      <div className="close-button" onClick={onButtonClicked}>x</div>
-    </div>
+  return(
+    
+      <div className="friend-list">
+     {
+       friends.map( x =>
+        <div className='friend-list-item'>
+          <Avatarbot name={friends}/>
+          { x.displayName }
+        </div>
+        )
+     }
+      </div>
+    
   )
 }
+
 
 export default Userlist;
