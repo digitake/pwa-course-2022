de06847b00155e65d0fcc524b4a405d6b585e47d
rattanakorn00 <rattanakorn.plan@bumail.net>
2022-05-07 09:32:29 +0700

    แก้บัค

--- MMK22/src/FriendList.js
+++ MMK22/src/FriendList.js
@@ -6,19 +6,20 @@ import { Link } from 'react-router-dom';
 import Titlebar from './components/Titlebar.js';
 
 function FriendList(){
-  const { userList } = useChatStateContext();
+  const { userList, imageDict } = useChatStateContext();
 
   return(
     <App>
       <div className="friend-list">
-      <Titlebar value="FriendList"/>
+        <Titlebar value="FriendList"/>
       {
         userList.map((item) => {
+          let img = imageDict[item.key];
           return (
             <Link to={`/privatechat/${item.key}`} key={item.key}>
               <div className={`friend-list-item ${item.position}`}>
-                <Avatar className="friend-avatar" name={item.displayName}/>
-              <div className="friend-display">{item.displayName}</div>
+              <Avatar className="friend-avatar" name={item.displayName} base64Image={img}/>
+                <div className='display-name'> {item.displayName}</div>
               </div>
             </Link>
           )
